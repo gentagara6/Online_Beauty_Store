7befbacaa0e173b2e520d2f2ed7c2d24cf84ba34
gentagara6 <genta.gara@student.uni-pr.edu>
2025-12-15 21:57:39 +0100

    webpage image changes

--- js/sproducts.js
+++ js/sproducts.js
@@ -1,66 +1,66 @@
 const products = [
 {
     id: 1,
-    name: "Mighty Bamboo Panthenol Cream",
-    price: 19.99,
-    img: "img/1.jpg",
-    images: ["img/13.png", "img/14.png", "img/15.png", "img/16.png"],
+    name: "Glazing Milk",
+    price: 32.00,
+    img: "img/glazing_milk.webp",
+    images: ["img/glazing_milk.webp", "img/sproducts/1-2.jpg", "img/sproducts/1-1.avif", "img/sproducts/1-3.webp"],
     description: "The Purito Seoul Mighty Bamboo Panthenol Cream is a powerful moisturizing cream suitable for daily use."
 },
 {
     id: 2,
-    name: "Hydro Soothing Cream",
-    price: 29.99,
-    img: "img/2.jpg",
-    images: ["img/2.jpg", "img/2.jpg", "img/2.jpg", "img/2.jpg"],
+    name: "Barrier Restore Cream",
+    price: 32.00,
+    img: "img/barrier_restore_cream.webp",
+    images: ["img/barrier_restore_cream.webp", "img/sproducts/2-1.webp", "img/sproducts/2-2.webp", "img/sproducts/2-3.webp"],
     description: "Hydrating cream suitable for sensitive skin, providing deep moisture and calming effects."
 },
 {
     id: 3,
-    name: "Relief Cream",
-    price: 29.99,
-    img: "img/3.jpg",
-    images: ["img/3.jpg", "img/3.jpg", "img/3.jpg", "img/3.jpg"],
+    name: "Peptide Glazing Fluid",
+    price: 32.00,
+    img: "img/peptide_glazing_fluid.webp",
+    images: ["img/peptide_glazing_fluid.webp", "img/sproducts/3-2.webp", "img/sproducts/3-1.jpg", "img/sproducts/3-3.webp"],
     description: "Barrier-repairing and calming cream for irritated skin, reducing redness and discomfort."
 },
 {
     id: 4,
-    name: "Centella Ampoule",
-    price: 34.99,
-    img: "img/4.jpg",
-    images: ["img/4.jpg", "img/4.jpg", "img/4.jpg", "img/4.jpg"],
+    name: "Pineapple Refresh Cleanser",
+    price: 32.00,
+    img: "img/pineapple_refresh.webp",
+    images: ["img/pineapple_refresh.webp", "img/sproducts/4-2.webp", "img/sproducts/4-1.jpg", "img/sproducts/4-3.jpg"],
     description: "Centella asiatica ampoule that soothes and strengthens skin, promoting healing and elasticity."
 },
 {
     id: 5,
-    name: "Sun Serum",
-    price: 39.99,
-    img: "img/5.jpg",
-    images: ["img/5.jpg", "img/5.jpg", "img/5.jpg", "img/5.jpg"],
+    name: "Espresso Lip Tint",
+    price: 20.00,
+    img: "img/espresso_lip_tint.webp",
+    images: ["img/espresso_lip_tint.webp", "img/sproducts/5-1.jpeg", "img/sproducts/5-2.webp", "img/sproducts/5-3.webp"],
     description: "Lightweight sun serum with SPF protection that hydrates and protects against UV damage."
 },
 {
     id: 6,
-    name: "Vitamin C Brightening Cream",
-    price: 29.99,
-    img: "img/3.jpg",
-    images: ["img/3.jpg", "img/3.jpg", "img/3.jpg", "img/3.jpg"],
+    name: "Raspberry Jelly Lip Tint",
+    price: 20.00,
+    img: "img/rasberry_lip_tint.webp",
+    images: ["img/rasberry_lip_tint.webp", "img/sproducts/6-1.webp", "img/sproducts/6-2.webp", "img/sproducts/6-3.jpeg"],
     description: "Brightening cream infused with vitamin C to enhance radiance and reduce dark spots."
 },
 {
     id: 7,
-    name: "Moisture Repair Lotion",
-    price: 24.99,
-    img: "img/2.jpg",
-    images: ["img/2.jpg", "img/2.jpg", "img/2.jpg", "img/2.jpg"],
+    name: "Ribbon Lip Tint",
+    price: 20.00,
+    img: "img/ribbon_lip_tint.webp",
+    images: ["img/ribbon_lip_tint.webp", "img/sproducts/7-1.webp", "img/sproducts/7-2.jpg", "img/sproducts/7-3.avif"],
     description: "Light lotion to restore moisture and strengthen the skin barrier."
 },
 {
     id: 8,
-    name: "Gentle Cleansing Foam",
-    price: 19.99,
-    img: "img/1.jpg",
-    images: ["img/1.jpg", "img/1.jpg", "img/1.jpg", "img/1.jpg"],
+    name: "Toast Lip Tint",
+    price: 20.00,
+    img: "img/toast_liptint.webp",
+    images: ["img/toast_liptint.webp", "img/sproducts/8-1.webp", "img/sproducts/8-2.jpg", "img/sproducts/8-3.jpg"],
     description: "Gentle cleansing foam that removes impurities while maintaining skin hydration."
 }
 ];
