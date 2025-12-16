8a32e128998d12256cc662f55231d9ec021bea5a
gentagara6 <genta.gara@student.uni-pr.edu>
2025-12-16 23:34:02 +0100

    layout changes

--- js/sproducts.js
+++ js/sproducts.js
@@ -5,7 +5,7 @@ const products = [
     price: 32.00,
     img: "img/glazing_milk.webp",
     images: ["img/glazing_milk.webp", "img/sproducts/1-2.jpg", "img/sproducts/1-1.avif", "img/sproducts/1-3.webp"],
-    description: "The Purito Seoul Mighty Bamboo Panthenol Cream is a powerful moisturizing cream suitable for daily use."
+    description: "Glazing Milk is a lightweight, milky-texture essence designed as the essential prep step in a skincare routine to deliver deep hydration, calm skin, and prepare your skin for serums, moisturizers, and makeup."
 },
 {
     id: 2,
@@ -13,7 +13,7 @@ const products = [
     price: 32.00,
     img: "img/barrier_restore_cream.webp",
     images: ["img/barrier_restore_cream.webp", "img/sproducts/2-1.webp", "img/sproducts/2-2.webp", "img/sproducts/2-3.webp"],
-    description: "Hydrating cream suitable for sensitive skin, providing deep moisture and calming effects."
+    description: "The Barrier Restore Cream e is a rich, yet lightweight cream moisturizer designed to comfort, soothe, and restore the skin barrier. It is formulated as an antioxidant peptide powerhouse that works over time to deliver hydrated, smoother, and softer skin."
 },
 {
     id: 3,
@@ -21,15 +21,15 @@ const products = [
     price: 32.00,
     img: "img/peptide_glazing_fluid.webp",
     images: ["img/peptide_glazing_fluid.webp", "img/sproducts/3-2.webp", "img/sproducts/3-1.jpg", "img/sproducts/3-3.webp"],
-    description: "Barrier-repairing and calming cream for irritated skin, reducing redness and discomfort."
+    description: "The Peptide Glazing Fluid is a multi-purpose, lightweight gel serum designed to give the skin a signature glazed donut finish. It functions as a hydrating and brightening step in your routine, creating a dewy, luminous look immediately while working to visibly plump the skin over time."
 },
 {
     id: 4,
-    name: "Pineapple Refresh Cleanser",
+    name: "Pineapple Cleanser",
     price: 32.00,
     img: "img/pineapple_refresh.webp",
     images: ["img/pineapple_refresh.webp", "img/sproducts/4-2.webp", "img/sproducts/4-1.jpg", "img/sproducts/4-3.jpg"],
-    description: "Centella asiatica ampoule that soothes and strengthens skin, promoting healing and elasticity."
+    description: "The Pineapple Refresh Cleanser is the first step in their skincare routine, a daily cleanser designed to be both effective and gentle, removing impurities without stripping the skin of moisture. It has a unique balm-to-lather texture and is formulated to leave the skin feeling clean, soft, bouncy, and hydrated post-rinse."
 },
 {
     id: 5,
@@ -37,7 +37,7 @@ const products = [
     price: 20.00,
     img: "img/espresso_lip_tint.webp",
     images: ["img/espresso_lip_tint.webp", "img/sproducts/5-1.jpeg", "img/sproducts/5-2.webp", "img/sproducts/5-3.webp"],
-    description: "Lightweight sun serum with SPF protection that hydrates and protects against UV damage."
+    description: "The Espresso Peptide Lip Tint is a nourishing lip formula that provides a hint of sheer, buildable color along with a rich, glossy finish. It's essentially a lip care product with the added benefit of a deep, warm hue."
 },
 {
     id: 6,
@@ -45,7 +45,7 @@ const products = [
     price: 20.00,
     img: "img/rasberry_lip_tint.webp",
     images: ["img/rasberry_lip_tint.webp", "img/sproducts/6-1.webp", "img/sproducts/6-2.webp", "img/sproducts/6-3.jpeg"],
-    description: "Brightening cream infused with vitamin C to enhance radiance and reduce dark spots."
+    description: "The Raspberry Jelly is a specific shade in the Peptide Lip Tint collection. It combines the deep nourishment of a lip treatment with a beautiful, sheer wash of color and a rich, glossy finish."
 },
 {
     id: 7,
@@ -53,7 +53,7 @@ const products = [
     price: 20.00,
     img: "img/ribbon_lip_tint.webp",
     images: ["img/ribbon_lip_tint.webp", "img/sproducts/7-1.webp", "img/sproducts/7-2.jpg", "img/sproducts/7-3.avif"],
-    description: "Light lotion to restore moisture and strengthen the skin barrier."
+    description: "The Ribbon Peptide Lip Tint by rhode is a nourishing lip formula that provides a hint of sheer, buildable color along with a rich, glossy finish. It's essentially a lip care product with the added benefit of a light, delicate pink hue."
 },
 {
     id: 8,
@@ -61,6 +61,6 @@ const products = [
     price: 20.00,
     img: "img/toast_liptint.webp",
     images: ["img/toast_liptint.webp", "img/sproducts/8-1.webp", "img/sproducts/8-2.jpg", "img/sproducts/8-3.jpg"],
-    description: "Gentle cleansing foam that removes impurities while maintaining skin hydration."
+    description: "The Toast Peptide Lip Tint by rhode is a nourishing lip formula that provides a hint of sheer, buildable color along with a rich, glossy finish. It's essentially a lip care product with the added benefit of a warm, sophisticated rose taupe hue."
 }
 ];
