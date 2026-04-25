fdd9f195594fa47ae8d481a789600eb28dd708ed
erapopovaa <era.popova@student.uni-pr.edu>
2026-04-25 19:25:23 +0200

    Update shop.php

--- pages/shop.php
+++ pages/shop.php
@@ -12,6 +12,19 @@
     ["id"=>8, "name"=>"Toast Lip Tint","price"=>20,"image"=>"toast_liptint.webp"]
    ];
 
+   $newProducts = [
+    ["name"=>"Sprinkle Blush","price"=>25,"image"=>"sprinkle_blush.webp"],
+    ["name"=>"Sun Soak Blush","price"=>25,"image"=>"sun_soak_blush.webp"],
+    ["name"=>"Peptide Lip Shape","price"=>24,"image"=>"peptide_lip_shape.webp"],
+    ["name"=>"Peptide Lip Shape Set","price"=>60,"image"=>"lip_shape_set.webp"],
+    ["name"=>"Scented_toast_tint","price"=>20,"image"=>"scented_toast_tint.webp"],
+    ["name"=>"Scented_ribbon_tint","price"=>20,"image"=>"scented_ribbon_tint.webp"],
+    ["name"=>"Scented_rj_tint","price"=>22,"image"=>"scented_rj_tint.webp"],
+    ["name"=>"Scented_espresso_tint","price"=>23,"image"=>"scented_espresso_tint.webp"],
+
+];
+
+
    usort($products,function ($a, $b) {
     return $a['price'] <=> $b['price'];
    });
@@ -45,6 +58,22 @@
               <?php endif; ?>
             <?php endforeach; ?>
         </div>
+
+        </section>
+
+        <section class="shopping" id="new">
+          <h2>New Arrivals</h2>
+
+          <div class="products">
+            <?php foreach($newProducts as $p): ?>
+              <div class="product">
+                <img src="../assets/img/<?php echo $p['image']; ?>">
+                <h3><?php echo $p['name']; ?></h3>
+                <p>$<?php echo $p['price']; ?></p>
+              </div>
+              <?php endforeach; ?>
+          
+          </div>
 
         </section>
         
