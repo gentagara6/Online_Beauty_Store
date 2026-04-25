3995c03c28350c5fdb72d57805dd3f3b35206b69
ernesamavraj <ernesa.mavraj@student.uni-pr.edu>
2026-04-25 21:04:16 +0200

    OOP ne faqe

---
+++ classes/BlogPost.php
@@ -0,0 +1,19 @@
+
+<?php
+
+class BlogPost {
+    public $title;
+    public $image;
+    public $shortText;
+    public $fullText;
+    public $date;
+
+    public function __construct($title, $image, $shortText, $fullText, $date) {
+        $this->title = $title;
+        $this->image = $image;
+        $this->shortText = $shortText;
+        $this->fullText = $fullText;
+        $this->date = $date;
+    }
+}
+?>
---
+++ classes/makeupproduct.php
@@ -0,0 +1,24 @@
+// makeupproduct.php
+
+<?php
+require_once 'product.php';
+
+class MakeupProduct extends Product {
+    private $shade;
+
+    public function __construct($name, $price, $brand, $shade,$image) {
+        //perdoret trashegimia
+        parent::__construct($name, $price, $brand, $image);
+        $this->shade = $shade;
+    }
+
+    public function getShade() {
+        return $this->shade;  
+    }
+
+    public function setShade($shade) {
+        $this->shade = $shade;
+    }
+}
+
+?>
